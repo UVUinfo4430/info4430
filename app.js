9fb7afaeb83fcc2b85192ca1b68b1c5c299a680d
ehbrowning <ehope00@gmail.com>
2022-03-16 14:30:26 -0600

    logo update

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ function show_home(){
     tag("canvas").innerHTML=` 
     <div class="center-screen">
     
-    <p><img height="${window.innerHeight * .6}" src="https://www.brookersicecream.com/wp-content/uploads/2018/08/brookers-logo-final-large.png"></p>
+    <p><img height="${window.innerHeight * .6}" src="https://iconape.com/wp-content/files/zu/190423/png/utah-valley-university-uvu-logo.png"></p>
     <div style="text-align:center"></div>
     
     
