6eab31a0ccf9cafa1c19c2aeeb83441afc742cdf
Emily Browning <10827952@uvu.edu>
2022-04-19 16:47:06 -0600

    working

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 //This function is used to point the client-side code to the Google App Script server that handles data requests. Every time the code in Google App Script is modified, the server must be deployed again. When that happens the code returned by the function must be updated.
 
 function gas_deployment_id(){return(
-'AKfycbx7QHw7gFzVXGOW6k4doK-DpohEoqdQ2QLK5XUg4A3d52jVEYDDUhrCRPaalcvYlLEo'
+'AKfycbyOIceBGAqG-S5mdiCV9bsZWLJfGcIgEfZhW_P-dOkMoNViixGURl8CFn7K6lsynlvm'
 )}
